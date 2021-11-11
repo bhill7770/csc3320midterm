20fb475052549730bed4c0e536b7719e061b548e
bhill7770 <91581195+bhill7770@users.noreply.github.com>
2021-11-10 22:00:48 -0500

    Create originalscript.sh

---
+++ originalscript.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#script.sh
+#Programming: Brianna Hill
+#Email: bhill28@student.gsu.edu
+
+function find_statments() { #To find function
+# Now to initialize array index counter 
+i=0 
+# Iterate for separation between periods. Read in array 
+while IFS =. read -r -a line
+do 
+IFS="."
+# Iterate each elements of array line for stat in ${line[@]}
+do 
+# Assigning elements to array statements
+statements[i]=$(echo $stat | sed 's/^ //g')
+# Increment counter
+i=$((i+1))
+done
+done < $1
+echo "There are total ${#statements[@]} statements"
+}
+# See if argument is passed
+if(($#==1))
+then
+#Now set file to $1
+file=$1
+else
+# Prompt user for a file 
+echo -n "Text file: "
+read file
+fi
+# To see if the file exists
+if [ -f $file ]
+then 
+# Prompt to call the function
+find_statements $file
+else
+# Prompt to display error and exit 
+echo "$file: file does not exist!"
+exit 1 
+fi 
