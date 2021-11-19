df79f3a1daaf9e20ad4b38f699562997dedbf4c4
bhill7770 <91581195+bhill7770@users.noreply.github.com>
2021-11-19 16:16:57 -0500

    Update calcupdated.sh

--- calcupdated.sh
+++ calcupdated.sh
@@ -1,33 +1,33 @@
 # !/bin/bash 
 
-echo "Please enter two numbers : "
+echo "Please enter two numbers : " #prompt user for input 
 
-read x1
-read x2
+read x1 #input 1
+read x2 #input 2
 
-echo "Enter a choice [1-4]  :"
+echo "Enter a choice [1-4]  :" #prompt user for input 
 
-echo "1. Add [+]"
-echo "2. Subtract [-]"
-echo "3. Divide [÷]"
-echo "4. Multiply [*]"
-echo "5. Invalid/Cancel"
+echo "1. Add [+]" # addition
+echo "2. Subtract [-]" #subtraction
+echo "3. Divide [÷]" #division
+echo "4. Multiply [*]" #multiplication
+echo "5. Invalid/Cancel" #cancel
 
-read uc
+read uc #scanned input
 
 case $uc in
 
-	1) res='echo $x1 + $x2 | bc'
+	1) res='echo $x1 + $x2 | bc' #method for adding 
                 ;;
-        2) res='echo $x1 - $x2 | bc'
+        2) res='echo $x1 - $x2 | bc' #method for subtracting
                 ;;
-        3) res='echo $x1 \* $x2 | bc'
+        3) res='echo $x1 \* $x2 | bc' #method for multiplying 
                 ;;
-        4) res='echo 'scale=2; $x1 / $x2 | bc'
+        4) res='echo 'scale=2; $x1 / $x2 | bc'  
 ;;
 
-esac
-echo "Result ; $res'
+esac 
+echo "Result : $res' //display result 
 
 
 
