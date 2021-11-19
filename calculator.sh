6214f6fc116803bf07b09d8ea5a193b3a454fd59
bhill7770 <91581195+bhill7770@users.noreply.github.com>
2021-11-19 16:27:48 -0500

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,19 +1,29 @@
 # !/bin/bash
 
-echo "Please enter two numbers : "
+echo "Please enter two numbers : " #prompt user input
 
-read x
-read y
+read x1 # input 1
+read x2 # input 2  
 
-echo "Please enter a choice [1-4]  :"
+echo "Please enter a choice [1-4]  :" # prompt user input 
 
-echo "1. Add [+]"
-echo "2. Subtract [-]"
-echo "3. Divide [÷]"
-echo "4. Multiply [*]"
+echo "1. Add [+]" # addition operation
+echo "2. Subtract [-]" # subtraction operation
+echo "3. Divide [÷]" # division operation 
+echo "4. Multiply [*]" # muliplication operation 
 
-read uc
+read uc 
 
+#method for calculator operations 
 case $uc in
 
-	1) res='echo $x + $y | bc'
+  1) res='echo $x1 + $x2 | bc'
+  ;;
+  2)res=`echo $x1 - $x2 | bc`
+  ;;
+  3)res=`echo $x1 \* $x2| bc`
+  ;;
+  4)res=`echo "scale=2; $x1 / $x2" | bc`
+  ;;
+esac
+echo "Result : $res"
