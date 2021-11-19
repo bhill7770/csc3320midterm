9d6178e5db6fd517065dd9abc63050dc672b5b24
bhill7770 <91581195+bhill7770@users.noreply.github.com>
2021-11-19 16:49:36 -0500

    Update phonebook.sh

--- phonebook.sh
+++ phonebook.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 
-	echo -n "Please enter a name: "
-        read name
+	echo -n "Please enter a name: " #prompt user for input
+        read name #read user input
 
-        echo -n "Please enter the address: "
-        read address
+        echo -n "Please enter the address: " #prompt user for input
+        read address #read user input
 
-        echo -n "Please the phone number: "
-        read phone_number
+        echo -n "Please the phone number: " #prompt user for input
+        read phone_number #read user input
 
         echo "$name ; $address ; $phone_number" >>book.txt
 
@@ -16,18 +16,18 @@
 
 #!/bin/sh
 
-	echo -n "Please enter the name of the person to find personal account  information: "
-        read query
+	echo -n "Please enter the name of the person to find personal account  information: " #prompt user for input
+        read query #read user input
 
-        echo "Name ; Address ; Phone number"
-        grep -i $query book.txt
+        echo "Name ; Address ; Phone number" #prompt user for input
+        grep -i $query book.txt 
 
 
 #!/bin/sh
 
 
-	echo -n "Enter the name of the person whose information  you want to delete: "
-        read name
+	echo -n "Enter the name of the person whose information  you want to delete: " #prompt user for input
+        read name #read user input
 
 
         sed -i '/$name/d' book.txt
@@ -37,23 +37,23 @@
 	exit=0
 	while [ $exit -ne 1 ]
         do
-            echo -e "add, find, delete, exit: "
+            echo -e "add, find, delete, exit: " #operations 
             read input
 
-            if [ "$input" = "add" ]
+            if [ "$input" = "add" ] #adding user input 
             then
                 ./path_to_the_addition_script/name_of_the_addition_script
 
-            elif [ "$input" = "find" ]
+            elif [ "$input" = "find" ] #finding user input
             then
                 ./path_to_the_find_script/name_of_the_find_script
-            elif [ "$input" = "del" ]
+            elif [ "$input" = "del" ] #deleting user input
             then 
                 ./path_to_the_find_script/name_of_the_find_script
-            elif [ "$input" = "del" ]
+            elif [ "$input" = "del" ] #deleting user input
             then
                 ./path_to_the_deletion_script/name_of_the_deletion_script
-            elif [ "$input" = "exit" ]
+            elif [ "$input" = "exit" ] 
             then
                 exit=1
             fi
